9a7a988095ee967796520c8eff6f775b501cd4d3
Calogero Lo Leggio <kalos@nerdrug.org>
2013-01-06 11:55:07 +0100

    added apps (utils.yml renamed) zsh and emacs configs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-ANSIBLE_DEST=$HOME/projects/devops/ansible/
+export ANSIBLE_HOME=$HOME/projects/devops/ansible
+export ANSIBLE_PLAY_DESK=$HOME/projects/devops/playbooks/desktop_
+export ANSIBLE_CONFIG=$HOME/Private/ansible_private/ansible.cfg
+
+export PATH=$ANSIBLE_HOME/bin:$PATH
+export PYTHONPATH=$ANSIBLE_HOME/lib:$PYTHONPATH
+export ANSIBLE_LIBRARY=$ANSIBLE_HOME/library:$MANPATH
+export MANPATH=$ANSIBLE_HOME/docs/man:$MANPATH
 
 sudo aptitude install -y python2.6 python-paramiko python-yaml python-jinja2 python-apt
-git clone git://github.com/ansible/ansible.git $ANSIBLE_DEST
+git clone git://github.com/ansible/ansible.git $ANSIBLE_HOME
 
-
+git clone git@github.com:kalos/ansible-desktop.git $ANSIBLE_PLAY_DESK
+cd $ANSIBLE_PLAY_DESK
+ansible-playbook -v setup.yml
